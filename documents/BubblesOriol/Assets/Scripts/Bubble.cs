6898d66feea30374b6d31037e7f8fd6e2bc53650
S0lRaK <carlosj.gc989@gmail.com>
2015-05-13 21:08:57 +0200

    diaIntenso

--- documents/BubblesOriol/Assets/Scripts/Bubble.cs
+++ documents/BubblesOriol/Assets/Scripts/Bubble.cs
@@ -1,20 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class Bubble : MonoBehaviour {
 
 	public Animator animator;
+	public float floatingSpeed = 0.005f;
+	private Queue<float> movements = new Queue<float>();
+	private bool isBouncing = false;
+	private Rigidbody2D rigidbody;
+
+	void setMovements(int amount) {
+
+		float shift;
+
+		for (int i = 0; movements.Count < amount; i++) {
+			
+			do {
+				
+				shift = Mathf.Round (Random.Range(-0.01f, 0.01f) * 100f) / 100f;
+				
+			} while (shift == 0f);
+			
+			for (int x = 0; x < Mathf.RoundToInt (Random.Range (50f, 100f)); x++) {
+				
+				movements.Enqueue (shift);
+				
+			}
+			
+		}
+
+	}
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+
 		animator = GetComponent<Animator> ();
+		rigidbody = GetComponent<Rigidbody2D> ();
+
+		setMovements (1000);
+
+		//int index = 0;
+
+		//foreach (float movement in movements) { Debug.Log(index + " " + movement); index++; }
+		
+
+
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+
+		Debug.Log ("Me toco, oh, sí!");
+
+		isBouncing = true;
+
+		Debug.Log (collision.relativeVelocity.magnitude);
 
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 
+		float shift;
+
+		//Physics2D.
+
+		if (movements.Count == 0) setMovements(1000);
+
+		shift = (float)movements.Dequeue ();
+
+		//if (!isBouncing) transform.localPosition = new Vector2 (transform.localPosition.x + shift, transform.localPosition.y + floatingSpeed);
+		if (!isBouncing) rigidbody.position = new Vector2 (rigidbody.position.x + shift, rigidbody.position.y + floatingSpeed);
+		
 		//if (Input.touches[0].phase == TouchPhase.Began || Input.GetButtonDown("Fire1")) {
 
 			//FireRocket();
@@ -28,6 +88,9 @@ public class Bubble : MonoBehaviour {
 		Debug.Log ("Holaaaaa");
 
 		animator.SetBool ("exploded", true);
+		Destroy (GetComponent<CircleCollider2D> ());
+
+		isBouncing = true;
 
 	}
 }
--- documents/BubblesOriol/Assets/Scripts/BubbleGenerator.cs
+++ documents/BubblesOriol/Assets/Scripts/BubbleGenerator.cs
@@ -7,16 +7,19 @@ public class BubbleGenerator : MonoBehaviour {
 	public Rigidbody2D GreenBubble;
 	public Rigidbody2D RedBubble;
 	public Rigidbody2D YellowBubble;
+	private float startPosition;
 
-	void newBubble() {
 
-		Debug.Log ("Nueva burbuja");
-<<<<<<< HEAD
-		Rigidbody2D bubble1 = (Rigidbody2D) Instantiate(BlueBubble, new Vector2 (-0.2f, -7.0f), Quaternion.identity);
+	void newBubble(float x) {
+	
+
+		//Debug.Log (Camera.main.aspect);
+
+		Rigidbody2D bubble1 = (Rigidbody2D) Instantiate(BlueBubble, new Vector2 (x, -7.0f), Quaternion.identity);
 
 		bubble1.GetComponent<Transform> ().localScale = new Vector2 (0.4f, 0.4f);
 		bubble1.gravityScale = -0.02f;
-
+		/*
 		Rigidbody2D bubble2 = (Rigidbody2D) Instantiate(GreenBubble, new Vector2 (-1.5f, -9.0f), Quaternion.identity);
 		
 		bubble2.GetComponent<Transform> ().localScale = new Vector2 (0.3f, 0.3f);
@@ -36,19 +39,29 @@ public class BubbleGenerator : MonoBehaviour {
 		
 		bubble5.GetComponent<Transform> ().localScale = new Vector2 (0.4f, 0.4f);
 		bubble5.gravityScale = -0.02f;
-=======
-		Rigidbody2D bubble = (Rigidbody2D) Instantiate(BlueBubble, new Vector2 (0.0f, 0.0f), Quaternion.identity);
+*/
+	}
 
-		bubble.GetComponent<Transform> ().localScale = new Vector2 (0.4f, 0.4f);
-		bubble.gravityScale = -0.2f;
->>>>>>> 41186e66c5883130161f9d82cba9f33b95d2b164
+	void Awake() {
+
+		startPosition = Camera.main.orthographicSize * -1;
 
 	}
 
 	void Start() {
 
 		Debug.Log ("Inicio");
-		newBubble();
+		StartCoroutine (generateBubbles ());
+
+	}
+
+	IEnumerator generateBubbles(){
+		while (true) {
+			newBubble (0.0f);
+			//newBubble (-1.0f);
+			
+			yield return new WaitForSeconds (5);	
+		}
 
 	}
 }
