e56c83342d5bd7c2a18e2dd44e7b93ba689c5815
S0lRaK <carlosj.gc989@gmail.com>
2015-05-28 01:13:01 +0200

    Music through scenes

---
+++ project/Sobbles/Assets/Scripts/Music.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		DontDestroyOnLoad(GameObject.FindWithTag("Music"));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
